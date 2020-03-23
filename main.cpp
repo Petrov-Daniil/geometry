1a7ada674f475d9fb2d7c86e94e9d515849e1724
Petrov-Daniil <zara123486@mail.ru>
2020-03-23 15:02:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
+#include <cstdlib>
 #include <cstdio>
 
 using namespace std;
-float P = 3.141592653589793238466;
 
-void circle(float x, float y, float r) {
+double P = 3.1415;
+
+void circle(double x, double y, double r) {
     float per = 2 * P * r;
     float area = P * pow(r, 2);
     cout << "circle(" << x << " " << y << ", " << r << ")" << endl <<
          " perimetr = " << per << endl << " area = " << area << endl;
 }
 
+struct circle {
+    string name;
+    string sc;
+    double x;
+    double y;
+    double r;
+} c;
+
 int main() {
-    float x, y, r;
-    cin >> x >> y >> r;
-    circle(x, y, r);
+    cout << "Введите фигуру: " << endl; //example circle( 0 0, 1.5)
+    cin>>c.name>>c.x>>c.y>>c.sc>>c.r;
+    circle(c.x, c.y, c.r);
     return 0;
 }
-
