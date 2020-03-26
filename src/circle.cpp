d3c82b90fb105866cd03e017e524396e24141494
Petrov-Daniil <zara123486@mail.ru>
2020-03-26 15:15:31 +0700

    Add function circle of main.cpp

---
+++ src/circle.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctype.h>
+#include <iostream>
+
+using namespace std;
+
+void circle(float x, float y, float r, char name[]) {
+    float per = 2 * P * r;
+    float area = P * pow(r, 2);
+    cout << name << endl;
+    cout << " \tperimetr = " << per << endl;
+    cout << "\tarea = " << area << endl;
+}
