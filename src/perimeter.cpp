11587f9916ecbbdf06e643341dd2be48971a9e73
Petrov-Daniil <zara123486@mail.ru>
2020-03-27 12:19:19 +0700

    Create function perimeterCircle

---
+++ src/perimeter.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctype.h>
+#include <iostream>
+
+using namespace std;
+
+float const P = 3.1415;
+
+void perimeter(float x, float y, float r)
+{
+    float per = 2 * P * r;
+    cout << " \tperimetr = " << per << endl;
+}
