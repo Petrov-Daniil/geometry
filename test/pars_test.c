724165f1aac654154f19ae965c850a4b73cac28b
Petrov-Daniil <zara123486@mail.ru>
2020-04-12 13:32:58 +0700

    Add test Notcircle pars_test.c

--- test/pars_test.c
+++ test/pars_test.c
@@ -8,3 +8,13 @@ CTEST(pars_name, circle)
     k = pars_test_name("circle(1 6, 8)");
     ASSERT_EQUAL(1, k);
 }
+
+CTEST(pars_name, notCircle)
+{
+    int k = pars_test_name("hjfhjfhjfj(1 0, 2)");
+    ASSERT_EQUAL(1, k);
+    k = pars_test_name("Circle(1 0, 2)");
+    ASSERT_EQUAL(1, k);
+    k = pars_test_name("");
+    ASSERT_EQUAL(1, k);
+}
