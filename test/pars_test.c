01db430fc7853b63d20b00d4318ea41e3c631c99
Petrov-Daniil <zara123486@mail.ru>
2020-04-12 13:32:14 +0700

    Add test circle pars_test.c

---
+++ test/pars_test.c
@@ -0,0 +1,10 @@
+#include "../src/pars.cpp"
+#include "ctest.h"
+
+CTEST(pars_name, circle)
+{
+    int k = pars_test_name("circle(1 4, 2)");
+    ASSERT_EQUAL(1, k);
+    k = pars_test_name("circle(1 6, 8)");
+    ASSERT_EQUAL(1, k);
+}
