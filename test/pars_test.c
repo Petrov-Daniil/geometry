ee13aa43f764bebad4876db8190c421c247a922e
Petrov-Daniil <zara123486@mail.ru>
2020-04-16 01:20:48 +0700

    Correct unit-test

--- test/pars_test.c
+++ test/pars_test.c
@@ -12,11 +12,11 @@ CTEST(pars_name, circle)
 CTEST(pars_name, notCircle)
 {
     int k = pars_test_name("hjfhjfhjfj(1 0, 2)");
-    ASSERT_EQUAL(1, k);
+    ASSERT_EQUAL(-1, k);
     k = pars_test_name("Circle(1 0, 2)");
-    ASSERT_EQUAL(1, k);
+    ASSERT_EQUAL(-1, k);
     k = pars_test_name("");
-    ASSERT_EQUAL(1, k);
+    ASSERT_EQUAL(-1, k);
 }
 
 CTEST(parsNumber, Isnumber)
@@ -34,27 +34,29 @@ CTEST(parsNumber, Isnumber)
     ASSERT_DBL_NEAR(2.2, a[2]);
     char name_three[] = "circle(1.2 0.5, 2.64)";
     pars(name_three, a);
-    ASSERT_EQUAL(1.2, a[0]);
-    ASSERT_EQUAL(0.5, a[1]);
+    ASSERT_DBL_NEAR(1.2, a[0]);
+    ASSERT_DBL_NEAR(0.5, a[1]);
     ASSERT_DBL_NEAR(2.64, a[2]);
 }
 
 CTEST(parsNumber, IsNotnumber)
 {
-    float a[3];
+    float a[3] = {0, 0, 0};
     char name_one[] = "circle(1 4, a)";
     pars(name_one, a);
     ASSERT_EQUAL(1, a[0]);
     ASSERT_EQUAL(4, a[1]);
-    ASSERT_EQUAL(2, a[2]);
+    ASSERT_EQUAL(0, a[2]);
+    a[0] = 0, a[1] = 0, a[2] = 0;
     char name_two[] = "circle(! g, 2.2)";
     pars(name_two, a);
-    ASSERT_EQUAL(1, a[0]);
+    ASSERT_DBL_NEAR(2.2, a[0]);
     ASSERT_EQUAL(0, a[1]);
-    ASSERT_DBL_NEAR(2.2, a[2]);
+    ASSERT_EQUAL(0, a[2]);
+    a[0] = 0, a[1] = 0, a[2] = 0;
     char name_three[] = "circle(///////)";
     pars(name_three, a);
-    ASSERT_EQUAL(1.2, a[0]);
-    ASSERT_EQUAL(0.5, a[1]);
-    ASSERT_DBL_NEAR(2.64, a[2]);
+    ASSERT_EQUAL(0, a[0]);
+    ASSERT_EQUAL(0, a[1]);
+    ASSERT_EQUAL(0, a[2]);
 }
